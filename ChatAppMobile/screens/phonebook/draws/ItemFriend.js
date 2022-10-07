a4771e04c3aae8c78d3b1512461dc8aa55bf88d3
ANTi1302 <porkoe3443334@gmail.com>
2022-10-07 21:12:59 +0700

    create listfriend

---
+++ ChatAppMobile/screens/phonebook/draws/ItemFriend.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+function ItemFriend(props) {}
+export default ItemFriend;
